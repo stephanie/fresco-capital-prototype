3dac391a2fc476137565b1408a15319b588f3db2
Stephanie Siaw <siaw.stephanie@gmail.com>
2014-10-09 18:57:59 +0800

    our companies page including business card js

--- js/theme-scripts.js
+++ js/theme-scripts.js
@@ -1,7 +1,27 @@
+//to maintain business card dimensions
+var $company_box = $('.company-box');
+
+function setCompanyBoxHeight() {
+  if ($company_box.length > 0) {
+    $width = $company_box.width();
+    $height = $width / 1.667;
+
+    $company_box.css('height', $height + 'px');
+  }
+}
+
 $(document).ready(function() {
-  
+
   $('.home-slider').slick({
 
   });
 
+  setCompanyBoxHeight();
+
 });// Ready
+
+$(window).resize(function() {
+
+  setCompanyBoxHeight();
+
+});// Window resize
