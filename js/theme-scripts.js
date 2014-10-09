c16049454b421bb4ffeeeaf2c4d1f7d8617dbe4d
Stephanie Siaw <siaw.stephanie@gmail.com>
2014-10-09 23:07:18 +0800

    home box js

--- js/theme-scripts.js
+++ js/theme-scripts.js
@@ -1,14 +1,31 @@
 //to maintain business card dimensions
 var $company_box = $('.company-box');
+var $perspective_box = $('.perspective-box');
+var $home_box = $('.home-box');
 
-function setCompanyBoxHeight() {
-  if ($company_box.length > 0) {
-    $width = $company_box.width();
-    $height = $width / 1.667;
+function setBoxHeight(element, ratio) {
+  if (element.length > 0) {
+    $width = element.width();
+    $height = $width / ratio;
 
-    $company_box.css('height', $height + 'px');
+    element.height($height);
   }
 }
+
+function alignBoxHeight(element) {
+  // Get an array of all element heights
+  var elementHeights = element.map(function() {
+    return $(this).height();
+  }).get();
+
+  // Math.max takes a variable number of arguments
+  // `apply` is equivalent to passing each height as an argument
+  var maxHeight = Math.max.apply(null, elementHeights);
+
+  // Set each height to the max height
+  element.height(maxHeight);
+}
+
 
 $(document).ready(function() {
 
@@ -16,12 +33,18 @@ $(document).ready(function() {
 
   });
 
-  setCompanyBoxHeight();
+  setBoxHeight($company_box, 1.667);
+  setBoxHeight($perspective_box, 1);
+
+  alignBoxHeight($home_box);
 
 });// Ready
 
 $(window).resize(function() {
 
-  setCompanyBoxHeight();
+  setBoxHeight($company_box, 1.667);
+  setBoxHeight($perspective_box, 1);
+
+  alignBoxHeight($home_box);
 
 });// Window resize
